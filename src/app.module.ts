1f024f6b54b702140f44ca3e4250eb1038d7895d
leon lishenga <lishengaleon@gmail.com>
2022-06-10 02:33:21 +0300

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoadingBays } from './placeholder/entities/LoadingBays';
 import { MeetingRooms } from './placeholder/entities/MeetingRooms';
@@ -11,15 +11,19 @@ import { PlaceholderModule } from './placeholder/placeholder.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '46.101.124.245',
-      port: 3306,
-      username: 'admin',
-      password: 'P1r1chon1.',
-      database: 'test_placeholder',
-      entities: [Properties, Warehouses, Offices, LoadingBays, MeetingRooms],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: 3306,
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [Properties, Warehouses, Offices, LoadingBays, MeetingRooms],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     PlaceholderModule,
   ],
